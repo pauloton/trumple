f4a25fb02b248c5637945b3017c00a7668d61778
pauloton <MAILPAULOCARVAJAL@GMAIL.COM>
2026-02-20 19:04:04 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,12 @@
-import './globals.css'
-
 export const metadata = {
-  title: 'Trump Trouble',
-  description: 'Sort the chaos. A weekly Trump timeline puzzle game.',
-}
+  title: "Trumple",
+  description: "Sort the chaos. A daily Trump timeline puzzle game.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
